4a691638b48dab7ed298127ef9441ef77690bc2e
AnaCarolinadaSilva <anacarolina0722@gmail.com>
2023-12-15 14:46:46 -0300

    Parte 2 - Tabuada
    Parte 2 - Tabuada

--- main.py
+++ main.py
@@ -22,3 +22,18 @@ def listaQuadrados(max : Optional[int] = 10):
        "max" : max,
        "quadrados" : quadrados
    }
+
+
+#Tabuada
+
+
+@app.get("/tabuada/{numero}")
+def listaTabuada(numero : int, start : Optional[int] = 1 , end : Optional[int] = 10):
+   tabuada = []
+   for i in range(start, end+1):
+       tabuada.append(numero*i)
+  
+   return {
+       "número" : numero,
+       "tabuada" : tabuada
+    }
